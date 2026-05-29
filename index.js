5752bd3fa3ba21d5f028b0e843d91a6b434cb5a5
alphagit88 <alphagit88@gmail.com>
2026-05-29 10:30:31 +0530

    aws

--- index.js
+++ index.js
@@ -90,10 +90,8 @@ app.use((err, req, res, next) => {
 
 const PORT = process.env.PORT || 5000;
 
-if (process.env.NODE_ENV !== 'production') {
-  app.listen(PORT, () => {
-    console.log(`🚀 Backend server running on http://localhost:${PORT}`);
-  });
-}
+app.listen(PORT, () => {
+  console.log(`🚀 Backend server running on http://localhost:${PORT}`);
+});
 
 module.exports = app;
